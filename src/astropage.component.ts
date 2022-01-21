47b699c22f52c0ad475c203f860f1eada08a8644
suresh-cr <suresh.i@cloudripples.io>
2022-01-21 20:35:27 +0530

    create angular files

---
+++ src/astropage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-astropage',
+  templateUrl: './astropage.component.html',
+  styleUrls: ['./astropage.component.css']
+})
+export class AstropageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
